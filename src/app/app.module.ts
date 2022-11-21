e6572cf9e9dc390d896d96b9264a17637f2fc4f4
Ravi Ranjan (HM0009821) <ravi.ranjan2@happiestminds.com>
2022-11-21 14:43:03 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ViewAppointmentComponent } from './view-appointment/view-appointment.component';
+import { PlaceAppointmentComponent } from './place-appointment/place-appointment.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule , Routes } from '@angular/router';
+
+
+const appRoutes: Routes =[		
+  {path:'contact' , component:ContactComponent},
+  {path:'footer', component:FooterComponent},
+  {path:'home' , component:HomeComponent},
+  {path:'placeAppointment', component:PlaceAppointmentComponent},
+  {path:'viewAppointment', component:ViewAppointmentComponent},
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewAppointmentComponent,
+    PlaceAppointmentComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/place-appointment/place-appointment.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-place-appointment',
+  templateUrl: './place-appointment.component.html',
+  styleUrls: ['./place-appointment.component.css']
+})
+export class PlaceAppointmentComponent  {
+
+  cool(){
+
+    alert("Successfully Added");
+
+    console.log("fasdf")
+
+  }
+
+  textBoxDisabled=false;
+amount:number=0;
+weeks:number=1;
+pack1:boolean=false;
+pack2:boolean=false;
+pack3:boolean=false;
+toggle(){
+  this.textBoxDisabled=false;
+  this.amount=400;
+  this.pack1=false;
+  this.pack3=false;
+  this.pack2=true;
+}
+toggle2(){
+  this.textBoxDisabled=true;
+  this.amount=500;
+  this.pack2=false;
+  this.pack3=false;
+  this.pack1=true;
+}
+toggle3(){
+  this.textBoxDisabled=false;
+  this.amount=300;
+  this.pack1=false;
+  this.pack2=false;
+  this.pack3=true;
+}
+onKey(event:any)
+{
+  if(event.target.value=="")
+  {
+    if(this.pack2==true)
+    {
+      this.amount=400;
+    }else if(this.pack3==true)
+    {
+      this.amount=300;
+    }
+    
+  }else{
+    this.amount=Number(event.target.value) * this.amount;
+  }
+  
+}
+
+}
+
